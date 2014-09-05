6ebc5776caeb0d6699a1030eb0ea43ce751cf0b5
Daniel Margala <dmargala@uci.edu>
2014-09-05 11:09:49 -0700

    clean up fitspec and encapsulate wavelength grid computations in fitter class

--- bosslya/fitter.py
+++ bosslya/fitter.py
@@ -3,21 +3,49 @@ import numpy as np
 import scipy.sparse
 import scipy.sparse.linalg
 
+import bosslya
+
 class ContinuumFitter():
-    def __init__(self, params, obsWaveCenters, restWaveCenters):
+    def __init__(self, params, obsWaveMin, obsWaveMax,
+        restWaveMin, restWaveMax, restNParams, verbose=False):
+        self.verbose = verbose
+
+        # initialize binning arrays
+        assert obsWaveMax > obsWaveMin, ('obsWaveMax must be greater than obsWaveMin')
+        self.obsWaveMin = obsWaveMin
+        self.obsWaveMax = obsWaveMax
+
+        self.obsWaveMinIndex = bosslya.getFiducialPixelIndexOffset(np.log10(obsWaveMin))
+        self.obsWaveMaxIndex = bosslya.getFiducialPixelIndexOffset(np.log10(obsWaveMax))
+
+        self.obsWaveCenters = bosslya.getFiducialWavelength(np.arange(self.obsWaveMinIndex,self.obsWaveMaxIndex+1))
+        self.obsNParams = len(self.obsWaveCenters)
+
+        if verbose:
+            print 'Observed frame bin centers span [%.2f:%.2f] with %d bins.' % (
+                self.obsWaveCenters[0],self.obsWaveCenters[-1],self.obsNParams)
+
+        assert restWaveMax > restWaveMin, ('restWaveMax must be greater than restWaveMin')
+        self.restWaveMin = restWaveMin
+        self.restWaveMax = restWaveMax
+        self.restNParams = restNParams
+        self.restWaveDelta = float(restWaveMax-restWaveMin)/restNParams
+        self.restWaveCenters = 0.5*self.restWaveDelta + np.linspace(
+            restWaveMin,restWaveMax,restNParams,endpoint=False)
+
+        if verbose:
+            print 'Rest frame bin centers span [%.2f:%.2f] with %d bins.' % (
+                self.restWaveCenters[0],self.restWaveCenters[-1],self.restNParams)
+
+        # the number of "model" pixels (excluding per target params)
         self.params = params
         self.nParams = len(params)
-        self.obsWaveCenters = obsWaveCenters
-        self.nObs = len(obsWaveCenters)
-        self.restWaveCenters = restWaveCenters
-        self.nRest = len(restWaveCenters)
-        # the number of "model" pixels (excluding per target params)
         self.nModelPixels = 0
         for param in params:
             if param['type'] is 'obs':
-                self.nModelPixels += self.nObs
+                self.nModelPixels += self.obsNParams
             elif param['type'] is 'rest':
-                self.nModelPixels += self.nRest
+                self.nModelPixels += self.restNParams
         # sparse matrix entry holders
         self.rowIndices = []
         self.colIndices = []
@@ -28,26 +56,50 @@ class ContinuumFitter():
         self.soln = None
         self.nconstraints = 0
 
-    def addObservation(self, logFlux, obsSlice, restSlice, weights=None):
+    def addObservation(self, target, flux, wavelength, ivar, unweighted=True):
         """
         Adds an observation to be fit. Each of logFlux, restSlice, restSlice should 
         have the same length. The obsSlice and restSlice specify the model pixel indices
         of the corresponding logFlux values.
         """
+
+        # this spectrum's wavelength axis pixel offset
+        offset = bosslya.getFiducialPixelIndexOffset(np.log10(wavelength[0]))
+
+        obsFiducialIndices = np.arange(offset,offset+len(wavelength))
+        obsFiducialWave = bosslya.getFiducialWavelength(obsFiducialIndices)
+
+        restWave = obsFiducialWave/(1+target.z)
+        restIndices = ((restWave - self.restWaveMin)/(self.restWaveMax - self.restWaveMin)*self.restNParams).astype(int)
+
+        # trim ranges to valid data
+        validbins = np.all((
+            obsFiducialIndices >= self.obsWaveMinIndex, 
+            obsFiducialIndices  < self.obsWaveMaxIndex, 
+            restIndices < self.restNParams, 
+            restIndices >= 0, 
+            flux > 0, ivar > 0), axis=0)
+
+        logFlux = np.log(flux[validbins])
         nPixels = len(logFlux)
-        assert len(logFlux) > 0, ('Empty flux array')
-        assert len(obsSlice) == nPixels and len(restSlice) == nPixels, (
-            'Input data array sizes do not match')
-        assert np.amax(obsSlice) < self.nObs and np.amax(restSlice) < self.nRest, (
+        if nPixels <= 0:
+            if self.verbose:
+                print 'No good pixels in relavant range on target %s (z=%.2f)' % (target, target.z)
+            return 0
+        restSlice = restIndices[validbins]
+        obsSlice = obsFiducialIndices[validbins]-self.obsWaveMinIndex
+
+        assert np.amax(obsSlice) < self.obsNParams and np.amax(restSlice) < self.restNParams, (
             'Invalid model index value')
 
-        # Save logFlux values
-        if weights is None:
-            sqrtw = np.ones(nPixels)
+        # compute weights
+        if unweighted:
+            weights = np.ones(nPixels)
         else:
-            assert len(weights) == nPixels, (
-                'Weights array size does not match data')
-            sqrtw = np.sqrt(weights)
+            weights = ivar[validbins]
+        sqrtw = np.sqrt(weights)
+
+        # Append logFlux values
         self.logFluxes.append(sqrtw*logFlux)
 
         # Each row corresponds to single flux value, the model matrix
@@ -63,16 +115,16 @@ class ContinuumFitter():
         for i,param in enumerate(self.params):
             if param['type'] is 'obs':
                 colIndices.append(colOffset+obsSlice)
-                colOffset += self.nObs
+                colOffset += self.obsNParams
             elif param['type'] is 'rest':
                 colIndices.append(colOffset+restSlice)
-                colOffset += self.nRest
+                colOffset += self.restNParams
             elif param['type'] is 'target':
                 colIndices.append(colOffset+self.nTargets*np.ones(nPixels))
                 colOffset += 1
         self.colIndices.append(np.concatenate(colIndices))
 
-        # The coefficients in the model matrix are the weights, unless a 'coef'
+        # The coefficients in the model matrix are the sqrt(weight), unless a 'coef'
         # function is specified in the param dictionary
         for i,param in enumerate(self.params):
             if 'coef' in param.keys():
@@ -84,30 +136,45 @@ class ContinuumFitter():
         # Increment the total number of pixel values and the number of observations
         self.nTotalPixels += nPixels
         self.nTargets += 1
+        return nPixels
 
-    def addConstraint(self, paramName, logFlux, index, coef):
+    def addConstraint(self, paramName, logFlux, wave, dwave, weight):
+        waveMin = wave - 0.5*dwave
+        waveMax = wave + 0.5*dwave
+        if paramName is 'T':
+            waves = self.obsWaveCenters
+        elif paramName is 'C':
+            waves = self.restWaveCenters
+        else:
+            assert False, ('Invalid constraint parameter')
 
-        assert len(index) == len(coef), ('index and coef must be same length')  
+        waveIndexRange = np.arange(np.argmax(waves > waveMin), np.argmax(waves > waveMax))
+        normCoefs = weight*np.ones(len(waveIndexRange))/len(waveIndexRange)
+
+        if self.verbose:
+            print 'Adding constraint: log%s([%.4f:%.4f]) = %.1f (range covers %d bins [%d:%d])' % (
+                paramName, waves[waveIndexRange[0]], waves[waveIndexRange[-1]], logFlux, 
+                len(normCoefs), waveIndexRange[0], waveIndexRange[-1])
 
         colOffset = 0
         for i,param in enumerate(self.params):
             if param['name'] is paramName:
-                self.colIndices.append(colOffset+index)
+                self.colIndices.append(colOffset+waveIndexRange)
             if param['type'] is 'obs':
-                colOffset += self.nObs
+                colOffset += self.obsNParams
             elif param['type'] is 'rest':
-                colOffset += self.nRest
+                colOffset += self.restNParams
             elif param['type'] is 'target':
                 colOffset += 1
 
-        self.coefficients.append(coef)
-        self.rowIndices.append(self.nTotalPixels*np.ones(len(index)))
+        self.coefficients.append(normCoefs)
+        self.rowIndices.append(self.nTotalPixels*np.ones(len(waveIndexRange)))
         self.logFluxes.append([logFlux])
 
         self.nTotalPixels += 1
         self.nconstraints += 1        
 
-    def fit(self, atol=1e-8, btol=1e-8, max_iter=100, sklearn=False, verbose=False):
+    def fit(self, atol=1e-8, btol=1e-8, max_iter=100, sklearn=False):
         """
         Does final assembly of the sparse matrix representing the model and performs least
         squares fit.
@@ -125,9 +192,9 @@ class ContinuumFitter():
         # convert the sparse matrix to compressed sparse column format
         self.model = model.tocsc()
 
-        if verbose:
-            print 'Number of transmission model params: %d' % self.nObs
-            print 'Number of continuum model params: %d' % self.nRest
+        if self.verbose:
+            print 'Number of transmission model params: %d' % self.obsNParams
+            print 'Number of continuum model params: %d' % self.restNParams
             print 'Number of targets: %d' % self.nTargets
             print 'Total model params: %d' % nModelPixels
             print 'Total number of flux measurements: %d' % self.nTotalPixels
@@ -138,14 +205,14 @@ class ContinuumFitter():
         if sklearn:
             from sklearn import linear_model
             regr = linear_model.LinearRegression()
-            if verbose:
+            if self.verbose:
                 print '... performing fit using sklearn.linear_model.LinearRegression ...\n'
             regr.fit(self.model, logFluxes)
             self.soln = regr.coef_
         else:
-            if verbose:
+            if self.verbose:
                 print '... sperforming fit using cipy.sparse.linalg.lsqr ...\n'
-            soln = scipy.sparse.linalg.lsqr(self.model, logFluxes, show=verbose,
+            soln = scipy.sparse.linalg.lsqr(self.model, logFluxes, show=self.verbose,
                 iter_lim=max_iter, atol=atol, btol=btol)
             self.soln = soln[0]
 
@@ -161,9 +228,9 @@ class ContinuumFitter():
         offset = 0
         for i,param in enumerate(self.params):
             if param['type'] is 'obs':
-                npixels = self.nObs
+                npixels = self.obsNParams
             elif param['type'] is 'rest':
-                npixels = self.nRest
+                npixels = self.restNParams
             elif param['type'] is 'target':
                 npixels = self.nTargets
             results[param['name']] = self.soln[offset:offset+npixels]
@@ -177,7 +244,7 @@ class ContinuumFitter():
         assert self.soln is not None, ('Can\'t request results before fitting')
         logFluxes = np.concatenate(self.logFluxes)
         residuals = logFluxes - self.model.dot(self.soln)
-        return np.dot(residuals,residuals)
+        return np.dot(residuals,residuals)/len(residuals)
 
     def getObservationChiSq(self, i):
         """
@@ -200,11 +267,39 @@ class ContinuumFitter():
 
         # calculate chisq
         residuals = logFluxes - model.dot(self.soln)
-        return np.dot(residuals, residuals)
+        return np.dot(residuals, residuals)/len(residuals)
 
 
     @staticmethod
     def addArgs(parser):
+        # transmission model wavelength grid options
+        parser.add_argument("--obsmin", type=float, default=3600,
+            help="transmission model wavelength minimum")
+        parser.add_argument("--obsmax", type=float, default=10000,
+            help="transmission model wavelength maximum")
+        # continuum model wavelength grid options
+        parser.add_argument("--restmin", type=float, default=850,
+            help="rest wavelength minimum")
+        parser.add_argument("--restmax", type=float, default=2850,
+            help="rest wavelength maximum")
+        parser.add_argument("--nrestbins", type=int, default=500,
+            help="number of restframe bins")
+        ####### constraints ########
+        parser.add_argument("--restnorm", type=float, default=1280,
+            help="restframe wavelength to normalize at")
+        parser.add_argument("--drestnorm", type=float, default=10,
+            help="restframe window size +/- on each side of restnorm wavelength")
+        parser.add_argument("--restnormweight", type=float, default=1e4,
+            help="norm constraint weight")
+        parser.add_argument("--obsnorm", type=float, default=5000,
+            help="obsframe wavelength to normalize at")
+        parser.add_argument("--dobsnorm", type=float, default=10,
+            help="obsframe window size +/- on each side of obsnorm wavelength")
+        parser.add_argument("--obsnormweight", type=float, default=1e4,
+            help="norm constraint weight")
+        ####### fit options #######
+        parser.add_argument("--unweighted", action="store_true",
+        help="perform unweighted least squares fit")
         parser.add_argument("--sklearn", action="store_true",
             help="use sklearn linear regression instead of scipy lstsq")
         ## scipy fit options
--- examples/fitspec.py
+++ examples/fitspec.py
@@ -27,40 +27,11 @@ def main():
         help="target list")
     parser.add_argument("-n","--ntargets", type=int, default=0,
         help="number of targets to use, 0 for all")
-    ####### fit options #######
-    parser.add_argument("--unweighted", action="store_true",
-        help="perform unweighted least squares fit")
     # z evolution options
     parser.add_argument("--alpha", action="store_true",
         help="add z evolution term")
     parser.add_argument("--beta", type=float, default=3.92,
         help="optical depth power law parameter")
-    # transmission model wavelength grid options
-    parser.add_argument("--transmin", type=float, default=3600,
-        help="transmission model wavelength minimum")
-    parser.add_argument("--transmax", type=float, default=10000,
-        help="transmission model wavelength maximum")
-    # continuum model wavelength grid options
-    parser.add_argument("--restmin", type=float, default=850,
-        help="rest wavelength minimum")
-    parser.add_argument("--restmax", type=float, default=2850,
-        help="rest wavelength maximum")
-    parser.add_argument("--nrestbins", type=int, default=500,
-        help="number of restframe bins")
-    ## continuum model constraint
-    parser.add_argument("--restnorm", type=float, default=1280,
-        help="restframe wavelength to normalize at")
-    parser.add_argument("--drestnorm", type=float, default=10,
-        help="restframe window size +/- on each side of restnorm wavelength")
-    parser.add_argument("--restnormweight", type=float, default=1e4,
-        help="norm constraint weight")
-    ## transmission model constraint
-    parser.add_argument("--obsnorm", type=float, default=5000,
-        help="obsframe wavelength to normalize at")
-    parser.add_argument("--dobsnorm", type=float, default=10,
-        help="obsframe window size +/- on each side of obsnorm wavelength")
-    parser.add_argument("--obsnormweight", type=float, default=1e4,
-        help="norm constraint weight")
     bosslya.ContinuumFitter.addArgs(parser)
     args = parser.parse_args()
 
@@ -74,34 +45,13 @@ def main():
     fitsPath = os.path.join(boss_root, boss_version)
 
     # read target list
-    targets = bosslya.readTargetList(args.input,[('ra',float),('dec',float),('z',float),('thingid',int),('sn',float)])
+    targets = bosslya.readTargetList(args.input,[('ra',float),('dec',float),('z',float),('thingid',int)])#,('sn',float)])
     ntargets = args.ntargets if args.ntargets > 0 else len(targets)
     targets = sorted(targets[:ntargets])
     if args.verbose: 
         print 'Read %d targets from %s' % (ntargets,args.input)
 
-    # initialize binning arrays
-    nobsbins = 4800
-    obsWaveCenters = bosslya.getFiducialWavelength(np.arange(nobsbins))
-
-    transminindex = np.argmax(obsWaveCenters > args.transmin)
-    transmaxindex = np.argmax(obsWaveCenters > args.transmax)
-    transWaveCenters = bosslya.getFiducialWavelength(np.arange(transminindex,transmaxindex+1))
-    ntransbins = len(transWaveCenters)
-    if args.verbose:
-        print 'Observed frame bin centers span [%.2f:%.2f] with %d bins.' % (
-            transWaveCenters[0],transWaveCenters[-1],ntransbins)
-
-    restmin = args.restmin
-    restmax = args.restmax
-    nrestbins = args.nrestbins
-    drest = float(restmax-restmin)/nrestbins
-    restWaveCenters = np.linspace(restmin,restmax,nrestbins,endpoint=False) + drest/2
-    if args.verbose:
-        print 'Rest frame bin centers span [%.2f:%.2f] with %d bins.' % (
-            restWaveCenters[0],restWaveCenters[-1],nrestbins)
-
-    # Initialize model
+    # prepare model
     params = []
     params.append({'name':'T','type':'obs'})
     params.append({'name':'C','type':'rest'})
@@ -115,7 +65,8 @@ def main():
         params.append({'name':'alpha','type':'rest','coef':alphaCoef})
 
     # Initialize fitter 
-    fitter = bosslya.ContinuumFitter(params, transWaveCenters, restWaveCenters)
+    fitter = bosslya.ContinuumFitter(params, args.obsmin, args.obsmax, 
+        args.restmin, args.restmax, args.nrestbins, verbose=args.verbose)
 
     if args.verbose:
         print 'Fit model initialized with %d model params.\n' % fitter.nModelPixels
@@ -140,69 +91,23 @@ def main():
         ivar = combined.ivar
         flux = combined.flux
 
-        # this spectrum's wavelength axis pixel offset
-        offset = bosslya.getFiducialPixelIndexOffset(np.log10(wavelength[0]))
-
-        obsindices = np.arange(offset,offset+combined.nPixels)
-        obswave = obsWaveCenters[obsindices]
-        restwave = obswave/(1+target.z)
-        restindices = ((restwave - restmin)/(restmax - restmin)*nrestbins).astype(int)
-
-        # trim ranges to valid data
-        validbins = np.all((
-            obsindices >= transminindex, 
-            obsindices  < transminindex+ntransbins, 
-            restindices < nrestbins, 
-            restindices >= 0, 
-            flux > 0, ivar > 0), axis=0)
-
-        logFlux = np.log(flux[validbins])
-        if len(logFlux) <= 0:
-            if args.verbose:
-                print 'Dropping target %s (z=%.2f) because there are no valid pixels in requested range' % (target, target.z)
-            continue
-        restSlice = restindices[validbins]
-        transSlice = obsindices[validbins]-transminindex#np.arange(offset,offset+combined.nPixels)[validbins]
-
-        # calculate weights
-        if args.unweighted:
-            weights = None
-        else:
-            weights = ivar[validbins]#**(2)/flux[validbins]**4
-
         # Add this observation to our fitter
-        fitter.addObservation(logFlux, transSlice, restSlice, weights)
-        fitTargets.append(target)
-        npixels.append(len(logFlux))
+        nPixelsAdded = fitter.addObservation(target, flux, wavelength, ivar, 
+            unweighted=args.unweighted)
+        if nPixelsAdded > 0:
+            fitTargets.append(target)
+            npixels.append(nPixelsAdded)
 
     if args.verbose:
         print ''
 
-    # Add constraint for continuum normalization
     if args.restnorm > 0:
-        normCMin = args.restnorm-args.drestnorm/2
-        normCMax = args.restnorm+args.drestnorm/2
-        normCRange = np.arange(np.argmax(restWaveCenters > normCMin), 
-            np.argmax(restWaveCenters > normCMax))
-        normCCoefs = np.ones(len(normCRange))/len(normCRange)
-        fitter.addConstraint('C', 0, normCRange, args.restnormweight*normCCoefs)
-        if args.verbose:
-            print 'Adding constraint: logC([%.4f:%.4f]) = %.1f (range covers %d continuum bins [%d:%d])' % (
-                restWaveCenters[normCRange[0]], restWaveCenters[normCRange[-1]], 0, len(normCCoefs), normCRange[0], normCRange[-1])
-
-    # Add constraint for transmission normalization
+        fitter.addConstraint('C', 0, args.restnorm, args.drestnorm, args.restnormweight)
     if args.obsnorm > 0:
-        normTMin = args.obsnorm-args.dobsnorm/2
-        normTMax = args.obsnorm+args.dobsnorm/2
-        normTRange = np.arange(np.argmax(transWaveCenters > normTMin), 
-            np.argmax(transWaveCenters > normTMax))
-        normTCoefs = np.ones(len(normTRange))/len(normTRange)
-        fitter.addConstraint('T', 0, normTRange, args.obsnormweight*normTCoefs)
-        if args.verbose:
-            print 'Adding constraint: logT([%.4f:%.4f]) = %.1f (range covers %d transmission bins [%d:%d])' % (
-                transWaveCenters[normTRange[0]], transWaveCenters[normTRange[-1]], 0, len(normTCoefs), normTRange[0], normTRange[-1])
+        fitter.addConstraint('T', 0, args.obsnorm, args.dobsnorm, args.obsnormweight)
+
     # run the fitter
-    results = fitter.fit(verbose=args.verbose, atol=args.atol, btol=args.btol, max_iter=args.max_iter, sklearn=args.sklearn)
+    results = fitter.fit(atol=args.atol, btol=args.btol, max_iter=args.max_iter, sklearn=args.sklearn)
     chisq = fitter.getChiSq()
 
     if args.verbose:
@@ -226,8 +131,8 @@ def main():
     outfile.create_dataset('model_indptr', data=fitter.model.indptr)
     outfile.create_dataset('model_shape', data=fitter.model.shape)
 
-    outfile.create_dataset('transWaveCenters', data=transWaveCenters)
-    outfile.create_dataset('restWaveCenters', data=restWaveCenters)
+    outfile.create_dataset('obsWaveCenters', data=fitter.obsWaveCenters)
+    outfile.create_dataset('restWaveCenters', data=fitter.restWaveCenters)
 
     outfile.create_dataset('targets', data=[str(target) for target in fitTargets])
     outfile.create_dataset('redshifts', data=[target.z for target in fitTargets])
