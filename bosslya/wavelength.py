9afbed8bc258246b07d2f3b3d36c5705e5fb4657
Daniel Margala <dmargala@uci.edu>
2014-09-08 14:47:10 -0700

    add lyman limit

--- bosslya/wavelength.py
+++ bosslya/wavelength.py
@@ -29,14 +29,14 @@ SkyLineList = [5578.5,5894.6,5894.6,7246.0]
 SkyLines = [Wavelength(value) for value in SkyLineList]
 
 
-QuasarEmissionLines = [972, 1026,1033.82,1215.24,1240.81,1305.53,
+QuasarEmissionLines = [912, 972, 1026,1033.82,1215.24,1240.81,1305.53,
     1335.31,1397.61,1399.8,1549.48,1640.4,1665.85,1857.4,
     1908.734,2326.0,2439.5,2799.117,3346.79,3426.85,3727.092,
     3729.875,3889.0,4072.3,4102.89,4341.68,4364.436,4862.68,
     4932.603,4960.295,5008.240,6302.046,6365.536,6529.03,
     6549.86,6564.61,6585.27,6718.29,6732.67]
 
-QuasarEmissionLabels = [r'Ly$\gamma$',r'Ly$\beta$','O VI',r'Ly$\alpha$','N V','O I','C II',
+QuasarEmissionLabels = ['LyLimit',r'Ly$\gamma$',r'Ly$\beta$','O VI',r'Ly$\alpha$','N V','O I','C II',
     'Si IV','O IV','C IV','He II','O III','Al III',
     'C III','C II','Ne IV','Mg II','Ne V','Ne VI','O II','O II',
     'He I','S II',r'H$\delta$',r'H$\gamma$','O III',r'H$\beta$','O III','O III','O III',
