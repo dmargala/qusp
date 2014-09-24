99464a82755b8dd0f84c47a7e14415e9ed81e1d6
Daniel Margala <dmargala@uci.edu>
2014-09-24 12:29:29 -0700

    add desimodel toplevel init file

---
+++ desimodel/__init__.py
@@ -0,0 +1,28 @@
+# License information goes here
+# -*- coding: utf-8 -*-
+"""
+=========
+desimodel
+=========
+
+A package for providing machine-readable data about the DESI focal plane and
+other hardware designs to simulations.
+"""
+#
+from __future__ import absolute_import, division, print_function, unicode_literals
+# The line above will help with 2to3 support.
+'''
+from desiUtil.install import version
+#
+# Set version string.
+#
+__version__ = version('$HeadURL: https://desi.lbl.gov/svn/code/desimodel/trunk/py/desimodel/__init__.py $')
+#
+# Clean up namespace
+#
+del version
+#
+#
+#
+import .focalplane as focalplane
+'''
