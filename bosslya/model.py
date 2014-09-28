d712cd4bca5c627c0d469dd7f5955b38c8b892a6
Daniel Margala <dmargala@uci.edu>
2014-09-28 13:21:10 -0700

    fix typo

--- bosslya/model.py
+++ bosslya/model.py
@@ -85,7 +85,7 @@ class ContinuumModel(object):
         obsFiducialWave = bosslya.wavelength.getFiducialWavelength(obsFiducialIndices)
 
         restWave = obsFiducialWave/(1+target.z)
-        restIndices = (restWave - self.restWaveMin)/(self.restWaveMax - self.restWaveMin)*self.restNParams).astype(int)
+        restIndices = np.floor((restWave - self.restWaveMin)/self.restWaveDelta).astype(int)
 
         # trim ranges to valid data
         validbins = np.all((
