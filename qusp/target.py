b768501f47508325451a65947ac57195043703d6
Daniel Margala <dmargala@uci.edu>
2014-10-02 14:57:04 -0700

    fix bug in Target init method

--- qusp/target.py
+++ qusp/target.py
@@ -17,7 +17,7 @@ class Target(dict):
         assert 'target' in self, 'Target: must have plate-mjd-fiber identifier key'
         plate,mjd,fiber = self['target'].split('-')
         self['plate'] = int(plate)
-        self['mjd'] = int(plate)
+        self['mjd'] = int(mjd)
         self['fiber'] = int(fiber)
     def toString(self):
         """
@@ -25,7 +25,7 @@ class Target(dict):
         """
         return self['target']
 
-def loadTargetData(filename, fields=[]):
+def loadTargetData(filename, fields=[], verbose=False):
     """
     Loads a target data from a text file. The first column must be plate-mjd-fiber
     target identifier. Use the fields argument to specify additional columns to
@@ -36,21 +36,25 @@ def loadTargetData(filename, fields=[]):
     """
     fields = [('target','S15',0)] + fields
     names, formats, cols = zip(*fields)
+    if verbose:
+        print 'Reading fields: %s' % (', '.join(names))
     targetData = np.genfromtxt(filename,dtype={'names':names,'formats':formats},usecols=cols)
 
     return [Target(dict(zip(targetData.dtype.names,target))) for target in targetData]
 
-def saveTargetData(filename, targets, fields=[]):
+def saveTargetData(filename, targets, fields=[], verbose=False):
     """
     Writes a list of targets to the provided file. By default, only the
     target plate-mjd-fiber is written to file. Use the fields argument
     to specify additional target fields to save.
     """
-    saveFields = ['target']
-    saveFields.extend(fields if type(fields) is list else [fields])
+    keys = ['target']
+    keys.extend(fields if type(fields) is list else [fields])
+    if verbose:
+        print 'Saving fields: %s' % (', '.join(keys))
     with open(filename, 'w') as outfile:
         for target in targets:
-            outfile.write(' '.join([str(target[key]) for key in saveFields])+'\n')
+            outfile.write(' '.join([str(target[key]) for key in keys])+'\n')
 
 def readTargetPlates(boss_path, targets, sort=True, verbose=False):
     """
@@ -62,13 +66,12 @@ def readTargetPlates(boss_path, targets, sort=True, verbose=False):
         targets = sorted(targets, key=lambda target: target['target'])
     currentlyOpened = None
     for target in targets:
-        plate, mjd, fiber = target['target'].split('-')
-        plateFileName = 'spPlate-%s-%s.fits' % (plate, mjd)
+        plateFileName = 'spPlate-%s-%s.fits' % (target['plate'], target['mjd'])
         if plateFileName != currentlyOpened:
             # load the spectrum file
             if currentlyOpened is not None:
                 spPlate.close()
-            fullName = os.path.join(boss_path,plate,plateFileName)
+            fullName = os.path.join(boss_path,str(target['plate']),plateFileName)
             if verbose:
                print 'Opening plate file %s...' % fullName
             spPlate = fits.open(fullName)
