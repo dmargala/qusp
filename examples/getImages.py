1354fcfae523f28835d48ffdea9de914051e74dc
Daniel Margala <dmargala@uci.edu>
2014-10-02 12:09:31 -0700

    update getImages script to use new target model

--- examples/getImages.py
+++ examples/getImages.py
@@ -8,8 +8,8 @@ def targetImageURL(target, w=256, h=256, opt='IOG', scale=.1):
     baseURL = 'http://skyservice.pha.jhu.edu/DR10/ImgCutout/getjpeg.aspx'
 
     options = {}
-    options['ra'] = target.ra
-    options['dec'] = target.dec
+    options['ra'] = target['ra']
+    options['dec'] = target['dec']
     options['width'] = w
     options['height'] = h
     options['scale'] = scale
@@ -25,14 +25,26 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-i","--input", type=str, default=None,
         help="target list")
+    parser.add_argument("--ra-col", type=int, default=1,
+        help="ra column")
+    parser.add_argument("--dec-col", type=int, default=2,
+        help="dec column")
+    parser.add_argument("--dry", action="store_true",
+        help="dry run")
+    parser.add_argument("--out-prefix", type=str, default='',
+        help="output prefix")
     args = parser.parse_args()
 
-    targets = qusp.readTargetList(args.input,fields=[('ra',float),('dec',float)])
+    fields = [('ra',float,args.ra_col),('dec',float,args.dec_col)]
+    targets = qusp.target.loadTargetData(args.input,fields)
 
     for target in targets:
         url = targetImageURL(target)
-        cmd = ['curl',url,'-o','%s.jpg'%str(target)]
-        subprocess.call(cmd)
+        cmd = ['curl',url,'-o',args.out_prefix+'%s.jpg'%target['target']]
+        if args.dry:
+            print ' '.join(cmd)
+        else:
+            subprocess.call(cmd)
 
 if __name__ == '__main__':
     main()
