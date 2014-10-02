1400e086b0b3b247dbf4970637e6faf133667705
Daniel Margala <dmargala@uci.edu>
2014-10-02 14:33:53 -0700

    clean up target module. implement Target class derived from dict

--- qusp/target.py
+++ qusp/target.py
@@ -1,50 +1,56 @@
-from collections import namedtuple
-from operator import itemgetter
+"""
+Provides support for working with BOSS targets.
+"""
+
 import numpy as np
 
 from astropy.io import fits
 import os
 
-class Target(namedtuple('Target',('plate','mjd','fiber'))):
-    def __str__(self):
-        return '%d-%d-%d' % (self.plate,self.mjd,self.fiber)
-    @classmethod
-    def fromString(cls, targetString):
-        plate, mjd, fiber = targetString.split('-')
-        return cls(int(plate), int(mjd), int(fiber))
-
-def readTargetList(filename, fields=[]):
-    namedFields = [('plate',int,0),('mjd',int,0),('fiber',int,0)] + fields
-    name, dtype, index = zip(*namedFields)
-
-    class Target(namedtuple('Target',name)):
-        def __str__(self):
-            return '%d-%d-%d' % (self.plate,self.mjd,self.fiber)
-        def attrs(self):
-            return self[3:]
-
-    targetList = []
-    with open(filename,'r') as infile:
-        for line in infile:
-            tokens = itemgetter(0,*index[3:])(line.strip().split())
-            namedTokens = tokens[0].split('-') + list(tokens)[1:]
-            for i in range(len(namedTokens)):
-                namedTokens[i] = dtype[i](namedTokens[i])
-            targetList.append(Target(*namedTokens))
-    return targetList
+class Target(dict):
+    def __init__(self,*arg,**kw):
+        """
+        Initializes a Target object. Parses the plate-mjd-fiber identifier
+        and adds separate fields to the dictionary.
+        """
+        super(Target, self).__init__(*arg, **kw)
+        assert 'target' in self, 'Target: must have plate-mjd-fiber identifier key'
+        plate,mjd,fiber = self['target'].split('-')
+        self['plate'] = int(plate)
+        self['mjd'] = int(plate)
+        self['fiber'] = int(fiber)
+    def toString(self):
+        """
+        Returns the standard plate-mjd-fiber string represntation of the target.
+        """
+        return self['target']
 
 def loadTargetData(filename, fields=[]):
+    """
+    Loads a target data from a text file. The first column must be plate-mjd-fiber
+    target identifier. Use the fields argument to specify additional columns to
+    read. Must specify a (name, type, column index) tuple for each field.
+
+    For example, to read a target list along with ra, dec, and z columns:
+        loadTargetData(<filename>, fields=[('ra',float,1),('dec',float,2),('z',float,3)])
+    """
     fields = [('target','S15',0)] + fields
     names, formats, cols = zip(*fields)
     targetData = np.genfromtxt(filename,dtype={'names':names,'formats':formats},usecols=cols)
 
-    return [dict(zip(targetData.dtype.names,target)) for target in targetData]
+    return [Target(dict(zip(targetData.dtype.names,target))) for target in targetData]
 
 def saveTargetData(filename, targets, fields=[]):
+    """
+    Writes a list of targets to the provided file. By default, only the
+    target plate-mjd-fiber is written to file. Use the fields argument
+    to specify additional target fields to save.
+    """
+    saveFields = ['target']
+    saveFields.extend(fields if type(fields) is list else [fields])
     with open(filename, 'w') as outfile:
-        fields = ['target'] + fields
         for target in targets:
-            outfile.write(' '.join([str(target[key]) for key in fields])+'\n')
+            outfile.write(' '.join([str(target[key]) for key in saveFields])+'\n')
 
 def readTargetPlates(boss_path, targets, sort=True, verbose=False):
     """
