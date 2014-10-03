ff7773508395f52b4fb1a2df93bba4ee68edf6e9
Daniel Margala <dmargala@uci.edu>
2014-10-03 14:18:43 -0700

    rename exptimes programs to perplate, split input target list into per plate target lists

---
+++ examples/perplate.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+import argparse
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+from astropy import units as u
+
+import qusp
+
+def main():
+    # parse command-line arguments
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--verbose", action="store_true",
+        help="print verbose output")
+    parser.add_argument("-o","--output", type=str, default=None,
+        help="hdf5 output filename")
+    ## targets to fit
+    parser.add_argument("-i","--input", type=str, default=None,
+        help="target list")
+    parser.add_argument("-n","--ntargets", type=int, default=0,
+        help="number of targets to use, 0 for all")
+    parser.add_argument("--z-col", type=int, default=3,
+        help="redshift column of input targetlist")
+    qusp.Paths.addArgs(parser)
+    args = parser.parse_args()
+
+    # setup boss data directory path
+    paths = qusp.Paths(**qusp.Paths.fromArgs(args))
+
+    # read target list
+    fields = [('z',float,args.z_col)]
+    targets = qusp.target.loadTargetData(args.input, fields=fields)
+    ntargets = args.ntargets if args.ntargets > 0 else len(targets)
+
+    currentObs = None
+    samePlateTargets = []
+    for target in targets:
+        plateMJD = '%s-%s' % (target['plate'],target['mjd'])
+        print plateMJD
+        if len(samePlateTargets) == 0 or currentObs == plateMJD:
+            currentObs = plateMJD
+            samePlateTargets.append(target)
+        else:
+            qusp.target.saveTargetData('%s-%s.txt' % (args.output,plateMJD), samePlateTargets, 'z')
+            samePlateTargets = []
+    qusp.target.saveTargetData('%s-%s.txt' % (args.output,plateMJD), samePlateTargets, 'z')
+
+    # fig = plt.figure(figsize=(8,8))
+    # ax = plt.subplot(111,polar=True)
+
+    # for target, spPlate in qusp.target.readTargetPlates(paths.boss_path,targets):
+    #     i = target['fiber'] - 1
+        # taibeg = spPlate[0].header['TAI-BEG']
+        # taiend = spPlate[0].header['TAI-END']
+        # nexp = spPlate[0].header['NEXP']/4
+        # ra = spPlate[0].header['RA']
+        # dec = spPlate[0].header['DEC']
+
+        # plateDiameter = (3.0*u.deg).to(u.rad)
+
+        # ra = (spPlate[5].data[i]['ra']*u.deg).to(u.rad)
+        # dec = (spPlate[5].data[i]['dec']*u.deg).to(u.rad)
+
+        # ax.scatter(ra, target['z'], alpha=.5, marker='.', lw=0)
+
+
+        #bars = ax.bar(ra, target['z'], width=plateDiameter.value, bottom=0.5, alpha=.1)
+
+    # plt.tick_params(labelleft='off')
+    # plt.ylim([0,4])
+    # plt.show()
+    # Use custom colors and opacity
+    # for r, bar in zip(radii, bars):
+    #     bar.set_facecolor(plt.cm.jet(r / 10.))
+    #     bar.set_alpha(0.5)
+
+if __name__ == '__main__':
+    main()
