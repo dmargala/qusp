67bf69c95421912dabb88b6312424e1e7092b5e1
Daniel Margala <dmargala@uci.edu>
2014-10-07 15:02:09 -0700

    update filter program to use new names

--- examples/filter.py
+++ examples/filter.py
@@ -1,27 +1,37 @@
 #!/usr/bin/env python
+"""
+A program to filter FITS catalogs into target lists
+"""
 import argparse
-import os
 
-import numpy as np
 from astropy.io import fits
 
 import qusp
 
 def main():
     # parse command-line arguments
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--verbose", action="store_true",
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        "--verbose", action="store_true",
         help="print verbose output")
-    parser.add_argument("-o","--output", type=str, default=None,
+    parser.add_argument(
+        "-o", "--output", type=str, default=None,
         help="optional output FITS file to write")
-    parser.add_argument("-i","--input", type=str, default=None,
+    parser.add_argument(
+        "-i", "--input", type=str, default=None,
         help="required input FITS file to read")
-    parser.add_argument("-s","--select", type=str, default=None,
-        help="tbdata selection string, ex: \"(tbdata['OBJTYPE'] == 'QSO') & (tbdata['Z'] > 2.1)\"")
-    parser.add_argument("--save", type=str, default=None,
+    parser.add_argument(
+        "-s", "--select", type=str, default=None,
+        help=("tbdata selection string, ex: "
+              "\"(tbdata['OBJTYPE'] == 'QSO') & (tbdata['Z'] > 2.1)\""))
+    parser.add_argument(
+        "--save", type=str, default=None,
         help="target list text file to save")
-    parser.add_argument("--annotate", type=str, default=None,
-        help="colon separated list of columns to annotate target list with, ex: \"ra:dec:z\"")
+    parser.add_argument(
+        "--annotate", type=str, default=None,
+        help=("colon separated list of columns to annotate target list with"
+              ", ex: \"ra:dec:z\""))
     args = parser.parse_args()
 
     assert args.input is not None, 'No input file specified'
@@ -35,23 +45,25 @@ def main():
         mask = eval(args.select)
         filtered = tbdata[mask]
         if args.verbose:
-            print 'Selecting %d rows matching: %s' % (len(filtered), args.select)
+            print 'Selecting %d rows matching: %s' % (
+                len(filtered), args.select)
     else:
         filtered = tbdata
         if args.verbose:
             print 'No selection specified (selecting all entries).'
 
     # construct list of fields to annotate target list
-    targets = [ ]
+    targets = []
     try:
         fields = args.annotate.split(':')
     except AttributeError:
-        fields = [ ]
+        fields = []
     # extract targets and fields from table data
-    for i,row in enumerate(filtered):
-        if args.verbose and (0 == ((i+1) % 1000)):
-            print '(processing input entry %d)' % (i+1)
-        targetstr = '-'.join([str(row[key]) for key in ['PLATE','MJD','FIBERID']])
+    for index, row in enumerate(filtered):
+        if args.verbose and (0 == ((index+1) % 1000)):
+            print '(processing input entry %d)' % (index+1)
+        targetstr = '-'.join(
+            [str(row[key]) for key in ['PLATE', 'MJD', 'FIBERID']])
         target = qusp.target.Target(target=targetstr)
         for field in fields:
             target[field] = row[field]
@@ -61,7 +73,7 @@ def main():
         # save target list as text file
         if args.verbose:
             print "Saving %d final targets to %s" % (len(targets), args.save)
-        qusp.target.saveTargetData(args.save,targets,fields)
+        qusp.target.save_target_list(args.save, targets, fields)
     # save filtered FITS file
     if args.output:
         if args.verbose:
