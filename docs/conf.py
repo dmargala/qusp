6757a1235793961b39fa17ab21a3a2175198b87a
Daniel Margala <dmargala@uci.edu>
2014-11-12 19:25:38 -0800

    fix to work with py2.7

--- docs/conf.py
+++ docs/conf.py
@@ -16,12 +16,12 @@ import sys
 import os
 
 # import mock
-from unittest.mock import MagicMock
+from mock import Mock as MagicMock
 
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
-            return Mock()
+        return Mock()
 
 MOCK_MODULES = [
     'numpy', 
