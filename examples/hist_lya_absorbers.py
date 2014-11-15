de0ff589a5bb53943536265ea40e639ce1a7896f
Daniel Margala <dmargala@uci.edu>
2014-11-15 00:20:36 -0800

    update names and add some stats to output

--- examples/hist_lya_absorbers.py
+++ examples/hist_lya_absorbers.py
@@ -25,8 +25,8 @@ def main():
         help="redshift column index")
     parser.add_argument("--output", type=str, default="absorber_redshifts.png",
         help="output file name")
-    parser.add_argument("--weighted", action="store_true",
-        help="use ivar weighting")
+    parser.add_argument("--unweighted", action="store_true",
+        help="don't use ivar weighting")
     qusp.Paths.add_args(parser)
     qusp.target.add_args(parser)
     args = parser.parse_args()
@@ -34,10 +34,8 @@ def main():
     # setup boss data directory path
     paths = qusp.Paths(**qusp.Paths.from_args(args))
     # read target list
-    targets = qusp.target.load_targets_from_args(args, fields=[('z', float, args.z_col)])
-
-    if args.verbose:
-        print 'using %d targets...' % len(targets)
+    target_list = qusp.target.load_target_list_from_args(args, 
+        fields=[('z', float, args.z_col)])
 
     forest_min = qusp.wavelength.Wavelength(args.forest_min)
     forest_max = qusp.wavelength.Wavelength(args.forest_max)
@@ -47,32 +45,38 @@ def main():
     absorber_weights = []
 
     # loop over targets
-    for target, combined in qusp.target.get_combined_spectra(targets, boss_path=paths.boss_path):
-
-        # print target.to_string(), target['z']
-
+    for target, combined in qusp.target.get_combined_spectra(target_list, boss_path=paths.boss_path):
+        # determine observed frame forest window
         obs_min = forest_min.observed(target['z'])
         obs_max = forest_max.observed(target['z'])
-
+        # find pixels values corresponding to this window
         pixel_min = combined.find_pixel(obs_min, clip=True)
-        if pixel_min == -1:
-            pixel_min = 0
         pixel_max = combined.find_pixel(obs_max, clip=True)
-        if pixel_max == combined.npixels:
-            pixel_max = combined.npixels-1
+        forest_slice = slice(pixel_min, pixel_max+1)
+        # calculate absorber redshifts and weights
+        absorber_z = combined.wavelength[forest_slice]/wave_lya - 1
+        absorber_weight = combined.ivar.values[forest_slice]
+        # save this absorbers for this target
+        absorber_redshifts.append(absorber_z)
+        absorber_weights.append(absorber_weight)
 
-        absorber_z = combined.wavelength[pixel_min:pixel_max+1]/wave_lya - 1
+    absorber_redshifts = np.concatenate(absorber_redshifts)
+    if args.unweighted:
+        absorber_weights = np.ones_like(absorber_redshifts)
+    else:
+        absorber_weights = np.concatenate(absorber_weights)
 
-        absorber_redshifts += absorber_z.tolist()
-        absorber_weights += (combined.ivar.values[pixel_min:pixel_max+1]).tolist()
+    if args.verbose:
+        print 'Number of absorbers: %d' % absorber_redshifts.shape[0]
+        print 'Mean number per target: %.2f' % (absorber_redshifts.shape[0]/len(target_list))
+        print 'Mean absorber redshift: %.4f' % np.mean(absorber_redshifts)
 
-    if not args.weighted:
-        absorber_weights = np.ones(len(absorber_redshifts))
-    fig = plt.figure(figsize=(8,6))
-    plt.hist(absorber_redshifts, weights=absorber_weights, bins=50, linewidth=.1, alpha=.5)
-    plt.xlabel(r'Absorber Redshifts')
-    plt.grid()
-    fig.savefig(args.output, bbox_inches='tight')
+    if args.output:
+        fig = plt.figure(figsize=(8,6))
+        plt.hist(absorber_redshifts, weights=absorber_weights, bins=50, linewidth=.1, alpha=.5)
+        plt.xlabel(r'Absorber Redshifts')
+        plt.grid()
+        fig.savefig(args.output, bbox_inches='tight')
 
 if __name__ == '__main__':
     main()
