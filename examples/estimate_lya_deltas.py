17ed6d77b98f5f390b5b2028d6f708ffd6a1b284
Daniel Margala <dmargala@uci.edu>
2014-11-17 16:19:00 -0800

    add continuum model classes

--- examples/estimate_lya_deltas.py
+++ examples/estimate_lya_deltas.py
@@ -15,6 +15,59 @@ import matplotlib.pyplot as plt
 import scipy.stats
 import scipy.interpolate
 
+class Continuum(object):
+    def __init__(self):
+        pass
+    def get_continuum(self, target, combined):
+        pass
+
+class LinearFitContinuum(object):
+    def __init__(self, specfits):
+        self.specfits = specfits
+        self.targets = specfits['targets'].value
+        self.redshifts = specfits['redshifts'].value
+        self.amp = specfits['amplitude'].value
+        self.nu = specfits['nu'].value   
+        self.rest_wave_centers = specfits['restWaveCenters'].value
+        self.obs_wave_centers = specfits['obsWaveCenters'].value
+        self.continuum = specfits['continuum'].value
+        self.transmission = specfits['transmission'].value 
+
+        self.tiltwave = specfits['nu'].attrs['tiltwave']
+
+        self.T = scipy.interpolate.UnivariateSpline(obsWaveCenters, transmission, s=0)
+        self.C = scipy.interpolate.UnivariateSpline(restWaveCenters, continuum, s=0)
+
+    def get_continuum(self, target, combined):
+        if not target['target'] in self.targets:
+            raise ValueError('Target not found in specified continuum results.')
+        target_index = np.argmax(target['target'] == self.targets)
+
+        assert target['z'] == self.redshifts[target_index]
+        target['nu'] = self.nu[target_index]
+        target['amp'] = self.amp[target_index]
+
+        z = target['z']
+        amp = target['amp']
+        nu = target['nu']
+
+        redshifted_waves = self.obs_wave_centers/(1+z)
+        continuum = amp/(1+z)*(redshifted_waves/self.tiltwave)**nu*self.T(self.obs_wave_centers)*self.C(redshifted_waves)
+
+        return qusp.SpectralFluxDensity(self.obs_wave_centers, continuum)
+
+class ConstantContinuum(object):
+    def __init__(self, norm_min, norm_max):
+        self.norm_min = norm_min
+        self.norm_max = norm_max
+        self.wavelength = qusp.wavelength.get_fiducial_wavelength(np.arange(4800))
+    def get_continuum(self, target, combined):
+        norm = combined.mean_flux(self.norm_min.observed(target['z']), self.norm_max.observed(target['z']))
+        if norm <= 0:
+            raise ValueError('norm <= 0')
+        continuum = norm*np.ones_like(self.wavelength)
+        return qusp.SpectralFluxDensity(self.wavelength, continuum)
+
 def main():
     # parse command-line arguments
     parser = argparse.ArgumentParser(
@@ -29,8 +82,10 @@ def main():
         help="wavelength of lya line")
     parser.add_argument("--z-col", type=int, default=1,
         help="redshift column index")
-    parser.add_argument("--output", type=str, default="",
+    parser.add_argument("--output", type=str, default="absorber",
         help="output file name")
+    parser.add_argument("--continuum", type=str, default="",
+        help="continuum to use")
     qusp.Paths.add_args(parser)
     qusp.target.add_args(parser)
     args = parser.parse_args()
@@ -49,14 +104,14 @@ def main():
     absorber_weights = []
     absorber_transmissions = []
 
-    norm_min = qusp.wavelength.Wavelength(1275)
-    norm_max = qusp.wavelength.Wavelength(1285)
+    if args.continuum:
+        continuum_model = LinearFitContinuum(args.continuum)
+    else:
+        continuum_model = ConstantContinuum(qusp.wavelength.Wavelength(1275), qusp.wavelength.Wavelength(1285))
 
     # loop over targets
     for target, combined in qusp.target.get_combined_spectra(target_list, boss_path=paths.boss_path):
-        continuum = combined.mean_flux(norm_min.observed(target['z']), norm_max.observed(target['z']))
-        if continuum <= 0:
-            continue
+        
         # determine observed frame forest window
         obs_min = forest_min.observed(target['z'])
         obs_max = forest_max.observed(target['z'])
@@ -64,6 +119,13 @@ def main():
         pixel_min = combined.find_pixel(obs_min, clip=True)
         pixel_max = combined.find_pixel(obs_max, clip=True)
         forest_slice = slice(pixel_min, pixel_max+1)
+
+        try:
+            continuum = continuum_model.get_continuum(target, combined)(combined.wavelength[forest_slice])
+        except ValueError:
+            print 'get_continuum: bad value for %s' % target.to_string()
+            continue
+
         # calculate absorber redshifts and weights
         absorber_z = combined.wavelength[forest_slice]/wave_lya - 1
         absorber_weight = combined.ivar.values[forest_slice]
@@ -110,7 +172,7 @@ def main():
     plt.hist(absorber_redshifts, weights=absorber_weights, bins=100, linewidth=.1, alpha=.5)
     plt.xlabel(r'Absorber Redshifts')
     plt.grid()
-    fig.savefig('absorber_redshifts.png', bbox_inches='tight')
+    fig.savefig(args.output+'-redshifts.png', bbox_inches='tight')
 
     ###################
     ###################
@@ -132,7 +194,7 @@ def main():
     plt.ylabel('transmission fraction')
     plt.colorbar()
 
-    fig.savefig('absorber_transmission.png', bbox_inches='tight')
+    fig.savefig(args.output+'-transmission.png', bbox_inches='tight')
 
     ###################
     ###################
@@ -140,9 +202,6 @@ def main():
     absorber_deltas = []
     # loop over targets
     for target, combined in qusp.target.get_combined_spectra(target_list, boss_path=paths.boss_path):
-        continuum = combined.mean_flux(norm_min.observed(target['z']), norm_max.observed(target['z']))
-        if continuum <= 0:
-            continue
         # determine observed frame forest window
         obs_min = forest_min.observed(target['z'])
         obs_max = forest_max.observed(target['z'])
@@ -150,6 +209,12 @@ def main():
         pixel_min = combined.find_pixel(obs_min, clip=True)
         pixel_max = combined.find_pixel(obs_max, clip=True)
         forest_slice = slice(pixel_min, pixel_max+1)
+
+        try:
+            continuum = continuum_model.get_continuum(target, combined)(combined.wavelength[forest_slice])
+        except ValueError:
+            print 'get_continuum: bad value for %s' % target.to_string()
+            continue
 
         absorber_z = combined.wavelength[forest_slice]/wave_lya - 1
 
@@ -167,13 +232,13 @@ def main():
     fig = plt.figure(figsize=(8,6))
     delta_bins = np.linspace(-5,5,100+1)
 
-    import sklearn.mixture
-    model = sklearn.mixture.GMM(2)
-    result = model.fit(absorber_deltas)
+    # import sklearn.mixture
+    # model = sklearn.mixture.GMM(2)
+    # result = model.fit(absorber_deltas)
 
-    logprob, responsibilities = result.score_samples(delta_bins)
-    pdf = np.exp(logprob)
-    pdf_individual = responsibilities * pdf[:, np.newaxis]
+    # logprob, responsibilities = result.score_samples(delta_bins)
+    # pdf = np.exp(logprob)
+    # pdf_individual = responsibilities * pdf[:, np.newaxis]
 
     # from scipy.stats import norm
     # mean_delta = result.means_[0, 0]
@@ -181,11 +246,11 @@ def main():
     # p1 = norm(mean_delta, np.sqrt(var_delta)).pdf(delta_bins)
 
     plt.hist(absorber_deltas, weights=absorber_weights, normed=True, bins=delta_bins, linewidth=.1, alpha=.5)
-    plt.plot(delta_bins, pdf, '-k')
-    plt.plot(delta_bins, pdf_individual, '--k')
+    # plt.plot(delta_bins, pdf, '-k')
+    # plt.plot(delta_bins, pdf_individual, '--k')
     plt.xlabel(r'Absorber Deltas')
     plt.grid()
-    fig.savefig('absorber_deltas', bbox_inches='tight')
+    fig.savefig(args.output+'-deltas.png', bbox_inches='tight')
 
 
 if __name__ == '__main__':
