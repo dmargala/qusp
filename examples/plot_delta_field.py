49a47c3faa009ca420e59610bc2829eabd3dbf44
Daniel Margala <dmargala@uci.edu>
2014-11-20 16:15:54 -0800

    add ra, dec distribution plot

--- examples/plot_delta_field.py
+++ examples/plot_delta_field.py
@@ -12,6 +12,9 @@ from mpl_toolkits.mplot3d import Axes3D
 import h5py
 import qusp
 
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
 def main():
     # parse command-line arguments
     parser = argparse.ArgumentParser(
@@ -66,7 +69,14 @@ def main():
     zmax = 7
     max_scale = 1.2
 
+    print ra.min(), ra.max()
+    print dec.min(), dec.max()
+
     if args.output:
+
+        ######################
+        # z distribution
+        ######################
 
         fig = plt.figure(figsize=(14,8))
         ax = plt.subplot(111, polar=True)
@@ -74,48 +84,80 @@ def main():
         ax.plot(ra, redshift, marker='.', markersize=1, lw=0)
 
         ax.set_rmax(max_scale*zmax)
-        az.set_rlabel_position(90)
+        ax.set_rlabel_position(90)
         ax.grid(True)
         fig.savefig(args.output+'flat.png')
 
-        ab_x = redshift * np.cos(ra) * np.sin(dec)
-        ab_y = redshift * np.sin(ra) * np.sin(dec)
-        ab_z = redshift * np.cos(dec)
+        #######################
+        # ra, dec distribution
+        #######################
+
+        fig = plt.figure(figsize=(14,8))
+        ax = plt.subplot(111, projection='hammer')
+
+
+        l = np.linspace(0, 360, 100, endpoint=False)
+        galactic_plane = SkyCoord(l=l*u.degree, b=np.zeros_like(l)*u.degree, frame='galactic').icrs
+
+        aitoff_ra = np.remainder(galactic_plane.ra.degree+90+360-0,360)
+        ind = aitoff_ra > 180
+        aitoff_ra[ind] -= 360
+        aitoff_ra = -aitoff_ra
+
+        ax.plot(np.radians(aitoff_ra), galactic_plane.dec.radian, lw=0, marker='.')
+
+        tick_labels = np.array([210, 240, 270, 300, 330, 0, 30, 60, 90, 120, 150])
+        #tick_labels = np.remainder(tick_labels+360+org,360)
+        ax.set_xticklabels(tick_labels) 
+
+        ax.plot(ra, dec, marker='.', markersize=1, lw=0)
+        ax.grid(True)
+        fig.savefig(args.output+'aitoff.png')
+
+        #######################
+        # 3D distribution
+        #######################
 
         fig = plt.figure(figsize=(14,8))
         ax = fig.add_subplot(111, projection='3d')
         #ax.set_aspect('equal')
 
+        # plot points in 3d
+        ab_x = redshift * np.cos(ra) * np.sin(np.pi/2-dec)
+        ab_y = redshift * np.sin(ra) * np.sin(np.pi/2-dec)
+        ab_z = redshift * np.cos(np.pi/2-dec)
         ax.scatter(ab_x, ab_y, ab_z, marker='.', s=.1)
 
-        ax.scatter(ab_x, ab_y, zs=0, zdir='z', marker='.', s=.1)
+        # 2d projections
+        ax.scatter(ab_x, ab_y, zs=-0.17*max_scale*zmax, zdir='z', marker='.', s=.1)
         ax.scatter(ab_y, ab_z, zs=-max_scale*zmax, zdir='x', marker='.', s=.1)
         ax.scatter(ab_x, ab_z, zs=+max_scale*zmax, zdir='y', marker='.', s=.1)
 
+        # draw shells
         def plot_shell(r):
+            # 3d 
             u = np.linspace(0, 2 * np.pi, 100)
             v = np.linspace(0, np.pi/2, 100)
-
             x = r * np.outer(np.cos(u), np.sin(v))
             y = r * np.outer(np.sin(u), np.sin(v))
             z = r * np.outer(np.ones(np.size(u)), np.cos(v))
             ax.plot_surface(x, y, z,  rstride=4, cstride=4, color='r', alpha=.05, edgecolor='None')
-
+            # 2d
             x = r * np.cos(u)
             y = r * np.sin(u)
-            ax.plot(x, y, zs=0, zdir='z', color='r', alpha=.2)
-            ax.plot(x[:50], y[:50], zs=-max_scale*zmax, zdir='x', color='r', alpha=.2)
-            ax.plot(x[:50], y[:50], zs=+max_scale*zmax, zdir='y', color='r', alpha=.2)
 
+            ind = (y > -0.17*max_scale*zmax)
+            ax.plot(x, y, zs=-0.17*max_scale*zmax, zdir='z', color='r', alpha=.4, marker='.', lw=0, ms=1)
+            ax.plot(x[ind], y[ind], zs=-max_scale*zmax, zdir='x', color='r', alpha=.4, marker='.', lw=0, ms=1)
+            ax.plot(x[ind], y[ind], zs=+max_scale*zmax, zdir='y', color='r', alpha=.4, marker='.', lw=0, ms=1)
         for r in np.arange(1, zmax+1, 1):
             plot_shell(r)
 
         ax.set_xlim3d(-max_scale*zmax, max_scale*zmax)
         ax.set_ylim3d(-max_scale*zmax, max_scale*zmax)
-        ax.set_zlim3d(0, max_scale*zmax)
-
+        ax.set_zlim3d(-0.17*max_scale*zmax, max_scale*zmax)
+        # set viewing angle
         ax.view_init(args.elev, args.azim)
-
         fig.savefig(args.output+'3d.png', bbox_inches='tight')
 
 
